a1b2a672af18ff340962adbb06e3fc264b1adbad
whagan <william_hagan@hotmail.com>
2021-02-09 10:13:38 -0600

    scanner

--- scanner/pypdf2.py
+++ scanner/pypdf2.py
@@ -1,16 +1,19 @@
 import sys
-sys.path.append('/home/will/will_workspace/python_workspace/scanner/venv/lib/python3.8/site-packages')
+sys.path.append('C://Users//nsecurity/project/venv//Lib//site-packages')
+#sys.path.append('/home/will/will_workspace/python_workspace/scanner/venv/lib/python3.8/site-packages')
 
 from PyPDF2 import PdfFileReader, PdfFileWriter
-pdf_doc = "/home/will/Desktop/test.pdf"
+pdf_doc = "X://Will\'s Folder//1099_A.pdf"
 pdf = PdfFileReader(pdf_doc)
 
 for page in range(pdf.getNumPages()):
     pdf_writer = PdfFileWriter()
     current_page = pdf.getPage(page)
-    if ('Will' in current_page.extractText()):
-        pdf_writer.addPage(current_page)
-output = "/home/will/Desktop/out2.pdf"
-with open(output, "wb") as out:
-    pdf_writer.write(out)
+    print(current_page.extractText())
+    #if ('Trust' in current_page.extractText()):
+        #pdf_writer.addPage(current_page)
+        #print("ONE")
+#output = "C://Users//nsecurity//Desktop//out.pdf"
+#with open(output, "wb") as out:
+    #pdf_writer.write(out)
 
--- scanner/scan.py
+++ scanner/scan.py
@@ -1,21 +1,22 @@
-import logging
+#import logging
 import os
+import re
 import sys
-sys.path.append('/home/will/will_workspace/python_workspace/scanner/venv/lib/python3.8/site-packages')
+#sys.path.append('/home/will/will_workspace/python_workspace/scanner/venv/lib/python3.8/site-packages')
 #sys.path.extend(['C:\\Users\\nsecurity\\project\\scanner\\scanner', 'C:\\Users\\nsecurity\\project\\scanner', 'C:\\Users\\nsecurity\\AppData\\Local\\Programs\\Python\\Python38-32\\Lib\\idlelib', 'C:\\Users\\nsecurity\\AppData\\Local\\Programs\\Python\\Python38-32\\python38.zip', 'C:\\Users\\nsecurity\\AppData\\Local\\Programs\\Python\\Python38-32\\DLLs', 'C:\\Users\\nsecurity\\AppData\\Local\\Programs\\Python\\Python38-32\\lib', 'C:\\Users\\nsecurity\\AppData\\Local\\Programs\\Python\\Python38-32', 'C:\\Users\\nsecurity\\AppData\\Local\\Programs\\Python\\Python38-32\\lib\\site-packages', 'C:\\Users\\nsecurity\\project\\venv\\Lib\\site-packages']
 #)
-#sys.path.append('C:\\Users\\nsecurity\\project\\venv\\Lib\\site-packages')
+sys.path.append('C:\\Users\\nsecurity\\project\\venv\\Lib\\site-packages')
 import pdfreader
 from pdfreader import SimplePDFViewer
 import re
 
-logger = logging.getLogger(__name__)
+#logger = logging.getLogger(__name__)
 
         
 
 def scan():
     file = '/home/will/Desktop/output.pdf'
-    #file = 'X:\\output.PDF'
+    file = 'X:\\output.PDF'
     try:
         fd = open(file, "rb")
     except OSError:
@@ -31,36 +32,41 @@ def scan():
         if 'CALL OUR 24 HOUR TELEPHONE BANKING' in page_string:
             continue
         if 'STATEMENT PERIOD' not in page_string:
-                continue
+            continue
         print(page_string)
-        page_pair = split(page_string)
-        page_top = page_pair[0]
-        page_bottom = page_pair[1]
-        st_per_rx = "STATEMENT PERIOD [\d]{1,2}/[\d]{1,2}/[\d]{4} To [\d]{1,2}/[\d]{1,2}/[\d]{4}"
-        st_period = re.findall(st_per_rx, page_top)
-        print(st_period)
-        bal_rx = "((?:\d{1,3}(?:,?\d{3})*?)\.\d{2}| \.\d{2})"
-        balances = [float(x) for x in [x.strip(' ').replace(',', '') for x in re.findall(bal_rx, page_top)[:4]]]
-        print(balances)
-        print("LENGTH: ", len(balances) == 4)
-        print("MATH: ", (balances[3] == round((balances[0] + balances[1] - balances[2]), 2)))
-
-
-def split(page_string):
-    if 'DEPOSITS' in page_string:
-        first_sub = 'DATE AMOUNT DATE AMOUNT DATE AMOUNT'
-        first_idx = page_string.index('DEPOSITS')
-        print(len(first_sub) + first_idx)
-        print(type(page_string[first_idx:first_idx + len(first_sub) + 2]))
-        if first_sub in page_string[first_idx:first_idx + len(first_sub) + 2]:
-            return (page_string.split('DEPOSITS DATE AMOUNT DATE AMOUNT DATE AMOUNT')[0],
-                    page_string.split('DEPOSITS DATE AMOUNT DATE AMOUNT DATE AMOUNT')[1])
-        else:
-            return logger.error('DEPOSITS FOUND')
-    else:
-        return logger.error('DEPOSITS NOT FOUND')
-    
+        get_period(page_string, r)
+        page_bottom = page_string.split('CHECKS CHECK NO DATE AMOUNT CHECK NO DATE AMOUNT')[1]
+        balances(page_string, r)
+        checks(page_string,  r)
         
+
+def get_period(page_string, r):
+    period_string = page_string.split('PAGE 1')[0]
+    st_per_rx = "STATEMENT PERIOD [\d]{1,2}/[\d]{1,2}/[\d]{4} To [\d]{1,2}/[\d]{1,2}/[\d]{4}"
+    st_period = re.findall(st_per_rx, period_string)
+    if (len(st_period) != 1):
+        print("ERROR: STATEMENT PERIOD")
+    print(st_period)
+
+def balances(page_string, r):
+    bal_string = page_string.split('CHECKS CHECK NO DATE AMOUNT CHECK NO DATE AMOUNT')[0]
+    bal_rx = "((?:\d{1,3}(?:,?\d{3})*?)\.\d{2}| \.\d{2})"
+    balances = [float(x) for x in [x.strip(' ').replace(',', '') for x in re.findall(bal_rx, bal_string)[:4]]]
+    if (len(balances) != 4):
+        print("MISSING BALANCE! Page ", r)
+    if (balances[3] != round((balances[0] + balances[1] - balances[2]), 2)):
+        print("BALANCE TOTALS OFF! Page ", r)
+
+
+def checks(page_string, r):
+    ck_string = re.split('CHECKS CHECK NO DATE AMOUNT CHECK NO DATE AMOUNT | OTHER DEBITS DATE AMOUNT DESCRIPTION', page_string)[1]
+    ck_rx = "\d{3,4}\*? [\d]{1,2}/[\d]{1,2}/[\d]{4} (?:\d{1,3}(?:,?\d{3})*?)\.\d{2}"
+    checks = re.findall(ck_rex, ck_string)
+
+def other_credits(page_string, r):
+    oc_string = re.split('OTHER CREDITS DATE AMOUNT DESCRIPTION | DAILY BALANCE INFORMATION DATE BALANCE DATE', page_string)[1]
+    oc_rex = "[\d]{1,2}/[\d]{1,2}/[\d]{4} ((?:\d{1,3}(?:,?\d{3})*?)\.\d{2}| \.\d{2}) 
+    
         
 
 
